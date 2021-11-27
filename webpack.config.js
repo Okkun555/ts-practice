07e06516f903e89923f49e8005ba68faaf947486
おっくん <49707341+yukioku63@users.noreply.github.com>
2021-11-27 21:38:13 +0900

    add:webpackの設定

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                loader: 'ts-loader',
+                test: /\.ts$/
+            }
+        ]
+    }
+}
